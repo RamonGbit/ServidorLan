1880a768057f31fd8fc1b1b49c7f78065ce40f0f
Abisaac1809 <abisaaccarmona@gmail.com>
2025-08-06 14:24:27 -0400

    feat: App class and Readme.md

--- main.py
+++ main.py
@@ -7,22 +7,24 @@ from Cli.Cli import CLI
 from Network.Network import Network
 from Persistence.ConfigManager import ConfigManager
 
-def main():
-    """Inicializa y ejecuta el simulador de red LAN.
+class App:
+    def run(self):
+        """Inicializa y ejecuta el simulador de red LAN.
 
-    Carga la configuración y lanza la interfaz de línea de comandos (CLI).
+        Carga la configuración y lanza la interfaz de línea de comandos (CLI).
 
-    Raises:
-        Exception: Si ocurre un error al cargar la configuración.
-    """
-    network = Network()
-    try:
-        ConfigManager.load_config(network, "running-config.json")
-        print("Configuración cargada desde running-config.json")
-    except Exception:
-        print("No se encontró configuración predeterminada; iniciando con una red vacía.")
-    cli = CLI()
-    cli.network = network
-    cli.run()
+        Raises:
+            Exception: Si ocurre un error al cargar la configuración.
+        """
+        network = Network()
+        try:
+            ConfigManager.load_config(network, "running-config.json")
+            print("Configuración cargada desde running-config.json")
+        except Exception:
+            print("No se encontró configuración predeterminada; iniciando con una red vacía.")
+        cli = CLI()
+        cli.network = network
+        cli.run()
 
-main()
+aplication = App()
+aplication.run()
